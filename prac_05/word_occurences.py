1de00fbde3afd78d066b84b6052be7081fb207dc
Calum_Maitland <calum.maitland00@gmail.com>
2019-09-03 10:13:29 +1000

    Sort and format outputs

--- prac_05/word_occurences.py
+++ prac_05/word_occurences.py
@@ -3,8 +3,13 @@ word_to_count = {}
 
 text = input("Text: ")
 string_list = text.split()
-print(string_list)
 for item in string_list:
     frequency = word_to_count.get(item, 0)
     word_to_count[item] = frequency + 1
-print(word_to_count)
+
+key_list = list(word_to_count.keys())
+key_list.sort()
+
+spacing = max((len(word) for word in key_list))
+for element in key_list:
+    print("{:{}} : {}".format(element, spacing, word_to_count[element]))
