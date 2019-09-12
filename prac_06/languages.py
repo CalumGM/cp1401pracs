a64b2eee70da230849646a9006d3f86ac317ba3c
Calum_Maitland <calum.maitland00@gmail.com>
2019-09-12 16:08:09 +1000

    finished used_cars and programming_languages

---
+++ prac_06/languages.py
@@ -0,0 +1,16 @@
+
+from prac_06.programming_languages import ProgrammingLanguage
+
+ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+
+print(python)
+
+languages = [ruby, python, visual_basic]
+print([str(language) for language in languages])
+
+print("\nThe dynamically typed languages are: ")
+dynamic_languages = [str(dynamic.name) for dynamic in languages if dynamic.is_dynamic()]
+for i in dynamic_languages:
+    print("{}".format(i))
