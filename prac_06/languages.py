e77e3cd2f6bed590b5035993ca08ab993d123eea
Calum_Maitland <calum.maitland00@gmail.com>
2019-09-19 10:18:04 +1000

    clean up print statements

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -5,12 +5,10 @@ ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
 python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
 visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
 
-print(python)
-
 languages = [ruby, python, visual_basic]
 print([str(language) for language in languages])
 
 print("\nThe dynamically typed languages are: ")
-dynamic_languages = [str(dynamic.name) for dynamic in languages if dynamic.is_dynamic()]
+dynamic_languages = [dynamic.name for dynamic in languages if dynamic.is_dynamic()]
 for i in dynamic_languages:
-    print("{}".format(i))
+    print(i)
