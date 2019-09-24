1efb17e425b858ee046e16ee44fa67793f52cea7
Calum_Maitland <calum.maitland00@gmail.com>
2019-09-24 11:49:53 +1000

    finished miles to km

--- prac_07/miles_to_km.py
+++ prac_07/miles_to_km.py
@@ -3,6 +3,8 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.core.window import Window
 
+CONST = 1.6
+
 
 class MilesToKmApp(App):
     def build(self):
@@ -13,15 +15,56 @@ class MilesToKmApp(App):
         return self.root
 
     def handle_button_up(self, value):
-        value += 1
+        valid_input = False
+        while not valid_input:
+            value = self.root.ids.input_text.text
+            try:
+                value = float(value)
+                value += 1
+                valid_input = True
+            except ValueError:
+                self.root.ids.input_text.text = "0"
+                print("Invalid Input")
+            except TypeError:
+                self.root.ids.input_text.text = "0"
+                print("Invalid Input")
         self.root.ids.input_text.text = str(value)
 
     def handle_button_down(self, value):
-        value -= 1
+        valid_input = False
+        while not valid_input:
+            value = self.root.ids.input_text.text
+            try:
+                value = float(value)
+                value -= 1
+                valid_input = True
+            except ValueError:
+                self.root.ids.input_text.text = "0"
+                print("Invalid Input")
+            except TypeError:
+                self.root.ids.input_text.text = "0"
+                print("Invalid Input")
         self.root.ids.input_text.text = str(value)
 
     def handle_convert(self, value):
-        value = value / 1.6
+
+        # value = float(value)
+        # value = value / 1.6
+        # self.root.ids.output_label.text = str(value)
+
+        valid_input = False
+        while not valid_input:
+            value = self.root.ids.input_text.text
+            try:
+                value = float(value)
+                value = value / CONST
+                valid_input = True
+            except ValueError:
+                self.root.ids.input_text.text = "0"
+                print("Invalid Input")
+            except TypeError:
+                self.root.ids.input_text.text = "0"
+                print("Invalid Input")
         self.root.ids.output_label.text = str(value)
 
 
